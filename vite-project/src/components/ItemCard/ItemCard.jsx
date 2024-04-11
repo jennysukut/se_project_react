8439a6239db4164878ee7a3529239d7439a27c61
JennySukut <jennysukut@gmail.com>
2024-04-11 14:44:48 +0200

    Add Array List for Rendering Cards

--- vite-project/src/components/ItemCard/ItemCard.jsx
+++ vite-project/src/components/ItemCard/ItemCard.jsx
@@ -1,37 +1,15 @@
 /*Clothing item cards, which are filtered based on the current weather. 
 Wrap the ItemCard component into the unordered list and use the filter() and map() methods.*/
 import "./ItemCard.css";
-import tShirt from "../../images/TShirt.svg";
-import shorts from "../../images/Shorts.svg";
-import cap from "../../images/Cap.svg";
-import sneakers from "../../images/Sneakers.svg";
 
-function ItemCard() {
+function ItemCard({ item }) {
   return (
-    <div className="itemCard">
-      <p className="itemCard__text">
-        Today is 100° F / You may want to wear nothing, but here are some
-        options:
-      </p>
-      <ul className="itemCard__list">
-        <li className="itemCard__card">
-          <div className="itemCard__title">TShirt</div>
-          <img src={tShirt} alt="" className="itemCard__card-image" />
-        </li>
-        <li className="itemCard__card">
-          <div className="itemCard__title">Shorts</div>
-          <img src={shorts} alt="" className="itemCard__card-image" />
-        </li>
-        <li className="itemCard__card">
-          <div className="itemCard__title">Cap</div>
-          <img src={cap} alt="" className="itemCard__card-image" />
-        </li>
-        <li className="itemCard__card">
-          <div className="itemCard__title">Sneakers</div>
-          <img src={sneakers} alt="" className="itemCard__card-image" />
-        </li>
-      </ul>
-    </div>
+    <>
+      <li className="itemCard__card">
+        <div className="itemCard__title">{item.name}</div>
+        <img src={item.link} alt={item.name} className="itemCard__card-image" />
+      </li>
+    </>
   );
 }
 
--- vite-project/src/components/Main/Main.jsx
+++ vite-project/src/components/Main/Main.jsx
@@ -1,12 +1,21 @@
 import WeatherCard from "../WeatherCard/WeatherCard";
 import ItemCard from "../ItemCard/ItemCard";
+import { defaultClothingItems } from "../../utils/constants";
 
 function Main() {
   return (
-    <div className="main">
+    <main className="main">
       <WeatherCard />
-      <ItemCard />
-    </div>
+      <p className="itemCard__text">
+        Today is 100° F / You may want to wear nothing, but here are some
+        options:
+      </p>
+      <ul className="itemCard__list">
+        {defaultClothingItems.map((item) => {
+          return <ItemCard key={item._id} item={item} />;
+        })}
+      </ul>
+    </main>
   );
 }
 
